4488cfcf1da25428a4fcf031009db0d651b0594d
FTDRefrain <yuanzhenyi@sjtu.edu.cn>
2019-03-14 18:03:47 -0400

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,4 +2,8 @@ import {createStore} from 'redux';
 
 import reducer from './reducer';
 
-export const store = createStore(reducer);
+export const store = createStore(
+	reducer, 
+	// reduc开发工具插件开启
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	);
