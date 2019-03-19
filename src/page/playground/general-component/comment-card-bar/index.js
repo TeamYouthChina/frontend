fa0ec6fd1d00b1983fa2f58d93815c8845405afc
FTDRefrain <yuanzhenyi@sjtu.edu.cn>
2019-03-19 18:29:23 -0400

    delete reducer

--- src/page/playground/general-component/comment-card-bar/index.js
+++ src/page/playground/general-component/comment-card-bar/index.js
@@ -15,11 +15,18 @@ class Comments extends React.Component {
   constructor(props) {
     super(props);
     this.state={
-      commentLists:null
+      commentLists:null,
+      showReplies: false,
+      showGive: false,
+      showCommentsText: '查看回复',
+      replyText: '回复',
+      allReplies: [],
     };
     this.text = Comments.i18n[languageHelper()];
     this.addComments = this.addComments.bind(this);
     this.getCurrentPage = this.getCurrentPage.bind(this);
+    this.showReplies = this.showReplies.bind(this);
+    this.giveReplies = this.giveReplies.bind(this);
   }
 
   componentDidMount() {
@@ -38,6 +45,23 @@ class Comments extends React.Component {
         create_at: '123',
         content: value,
       }, ...this.state.commentLists]
+    });
+  }
+  showReplies(){
+    let reply = !this.state.showReplies;
+    let text = this.state.showCommentsText === '查看回复' ? '收起回复' : '查看回复';
+    this.setState({
+      showReplies:reply,
+      showCommentsText:text
+    });
+  }
+
+  giveReplies(){
+    let show = !this.state.showGive;
+    let text = this.state.replyText === '回复' ? '取消回复' : '回复';
+    this.setState({
+      showGive:show,
+      replyText:text
     });
   }
 
@@ -103,6 +127,7 @@ Comments.propTypes = {
   commentsType: PropTypes.string.isRequired,
   
   showComments: PropTypes.func.isRequired,
+  
 };
 
 Comments.i18n = [
