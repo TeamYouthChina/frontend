5ae6daa34edda46585632e01660a60f271348cd6
FTDRefrain <yuanzhenyi@sjtu.edu.cn>
2019-03-26 18:46:30 -0400

    video detail

--- src/page/video/index.js
+++ src/page/video/index.js
@@ -6,31 +6,60 @@ import {Redirect} from 'react-router-dom';
 import {languageHelper} from '../../tool/language-helper';
 import {removeUrlSlashSuffix} from '../../tool/remove-url-slash-suffix';
 
+import VideoDes from './containers/videoDes';
+import Comments from './components/comment-card-bar';
+import data from './data';
+
 class VideoReact extends React.Component {
   constructor(props) {
     super(props);
     // state
-    this.state = {};
+    this.state = {
+      backend:null,
+      commentsText:'1条评论'
+    };
     // i18n
     this.text = VideoReact.i18n[languageHelper()];
   }
+
+  componentDidMount() {
+    this.setState({
+      backend:data.content
+    });
+  }
+
+  getCurrentPage(){}
 
   render() {
     const pathname = removeUrlSlashSuffix(this.props.location.pathname);
     if (pathname) {
       return (<Redirect to={pathname} />);
     }
-    return (
+    return (this.state.backend !== null) ? (
       <div>
-        <div
-          className="cell-wall"
-        >
+        <VideoDes
+          tags={this.state.backend.tags}
+          user={this.state.backend.user}
+          description={this.state.backend.description}
+          commentsText={this.state.commentsText}
+          videoTitle={this.state.backend.videoTitle}
+        />
+        <div style={{marginTop:'3.28vw'}} 
+             className="cell-wall">
           <div
             className="cell-membrane"
           >
-
+            <Comments
+              getCurrentPage={this.getCurrentPage}
+              commentsText={this.state.commentsText}
+              commentsType={'answer'}
+            />
           </div>
         </div>
+      </div>
+    ) : (
+      <div>
+        loading
       </div>
     );
   }
