a671cd0ec0c1cde185cb44a9abb7f30d2ecb2950
Zepei Xu <xuzepei19950617@gmail.com>
2019-04-07 14:00:47 -0400

    pass appid and usrsig to iframe through url parameters

--- src/page/playground/zepei/index.js
+++ src/page/playground/zepei/index.js
@@ -23,6 +23,9 @@ class ZepeiReact extends React.Component {
     if (pathname) {
       return <Redirect to={pathname} />;
     }
+    let id = 1400184624;
+    let sig = "eJxlj1FPgzAYRd-5FYTXGfdRqFlM9oAMJ0lxIW7G*UIaWliZQG3LmFv870bUiPG*nnNzc8*WbdvOmjxc0jxvu8Zk5k1yx762HXAufqGUgmXUZJ5i-yA-SqF4RgvD1QBdjDECGDuC8caIQnwbG82VP8Ka7bNh46vvA7gz-wr9UUQ5wCRKw3jJp3HxWAXBiTAiYUHyoDvBKix36eRusSTlfVSVU3zc40MgbspeSzl5UUlDSbjqk81a*W0dv1b0dkujZ9W1qE41PO22-Xw*mjSi5j*HwMPIn40vHbjSom0GAYGLXeTBZxzr3foAMUZdvg__";
+    let imURL = `http://127.0.0.1:8080?id=${id}&sig=${sig}`
     return (
       <div>
         <div className="cell-wall">
@@ -30,10 +33,10 @@ class ZepeiReact extends React.Component {
             <div style={{ textAlign: 'center', padding: '30px 30px', boxSizing: 'border-box' }}>
               <iframe
                 frameBorder="0"
-                src="http://127.0.0.1:8080?test1=value1&test2=value2"
+                src={imURL}
                 style={{
-                  height: `${1032 * 0.6 / 1280 * 100}vw`,
-                  width: `${1761 * 0.6 / 1280 * 100}vw`,
+                  height: `${1032 * 0.55 / 1280 * 100}vw`,
+                  width: `${1761 * 0.55 / 1280 * 100}vw`,
                 }}
               />
             </div>
