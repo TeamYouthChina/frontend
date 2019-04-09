07768a8abff80af27bc859073e4aa83d5a45c0b0
FTDRefrain <yuanzhenyi@sjtu.edu.cn>
2019-04-09 13:30:21 -0400

    add style

--- src/page/question/containers/question-des/index.js
+++ src/page/question/containers/question-des/index.js
@@ -8,53 +8,50 @@ import {languageHelper} from '../../../../tool/language-helper';
 import classes from './question-des.module.css';
 
 const QuestionDes = (props) => (
-  <div>
-    <MDBCol className={classes.mdbCol}>
-      <div className={classes.titleWrapper}>
-        {props.tags.map((item) => {
-          return (
-            <span className={classes.tagWrapper} key={item}>
-              <button className={classes.btnStyle}>
-                {item}
-              </button>
-            </span>
-          );
-        })}
-        <div className={classes.readingWrapper}>
-          <span className={classes.viewSpanStyle}>
-            <MDBIcon className={classes.viewIcon} far icon="eye" />
-              被浏览  <strong className={classes.readingColor}>1872W</strong>
-          </span>
-        </div>
-      </div>
-      <br />
-      <p className={classes.questionTitle}>{props.content.title}</p>
+  <MDBCol className={classes.mdbCol}>
+    {/*<div className={classes.titleWrapper}>*/}
+    {/*{props.tags.map((item) => {*/}
+    {/*return (*/}
+    {/*<span className={classes.tagWrapper} key={item}>*/}
+    {/*<button className={classes.btnStyle}>*/}
+    {/*{item}*/}
+    {/*</button>*/}
+    {/*</span>*/}
+    {/*);*/}
+    {/*})}*/}
+    {/*<div className={classes.readingWrapper}>*/}
+    {/*<span className={classes.viewSpanStyle}>*/}
+    {/*<MDBIcon className={classes.viewIcon} far icon="eye" />*/}
+    {/*被浏览  <strong className={classes.readingColor}>1872W</strong>*/}
+    {/*</span>*/}
+    {/*</div>*/}
+    {/*</div>*/}
+    <br />
+    <p className={classes.questionTitle}>{props.content.title}</p>
 
-      <p className={classes.questionDetail}>
-        {/*dangerouslySetInnerHTML={{__html: props.editorState.toHTML(props.content.description)}}*/}
-        {props.content.detail}
-      </p>
-      <div>
-        <button className={classes.btnAnswer}>
-          {props.text.toFocus}
-        </button>
-        <button className={classes.btnFocus}>
-          +
-          <Link to={`/question/${props.questionId}/answer/create`}>关注问题</Link>
-        </button>
-        <button className={classes.btnOthers}>
-          <MDBIcon className={classes.btnIcon} icon="thumbs-up"/>{props.text.toInvite}
-        </button>
-        <button className={classes.btnOthers}>
-          <MDBIcon className={classes.btnIcon} icon="comment"/>{props.text.pinglun}
-        </button>
-        <button className={classes.btnOthers}>
-          <MDBIcon className={classes.btnIcon} icon="share" />{props.text.share}
-        </button>
-      </div>
-    </MDBCol>
-
-  </div>
+    <p className={classes.questionDetail}>
+      {/*dangerouslySetInnerHTML={{__html: props.editorState.toHTML(props.content.description)}}*/}
+      {props.content.detail}
+    </p>
+    <div>
+      <button className={classes.btnAnswer}>
+        {props.text.toFocus}
+      </button>
+      <button className={classes.btnFocus}>
+        +
+        <Link to={`/question/${props.questionId}/answer/create`}>关注问题</Link>
+      </button>
+      <button className={classes.btnOthers}>
+        <MDBIcon className={classes.btnIcon} icon="thumbs-up" />{props.text.toInvite}
+      </button>
+      <button className={classes.btnOthers}>
+        <MDBIcon className={classes.btnIcon} icon="comment" />{props.text.pinglun}
+      </button>
+      <button className={classes.btnOthers}>
+        <MDBIcon className={classes.btnIcon} icon="share" />{props.text.share}
+      </button>
+    </div>
+  </MDBCol>
 );
 
 const i18n = [
