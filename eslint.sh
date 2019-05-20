b5bdca0534fcf7b5b8e2cf6ae3ad44837bdf93ca
Zhicheng Zhang <zzc-tongji@users.noreply.github.com>
2019-05-20 11:01:10 +0800

    There are still some bugs.
    See https://segmentfault.com/q/1010000019232807 for details.

--- eslint.sh
+++ eslint.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-set -u
-SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+# NOTE:
+# 
+# 1. The script should be executed on the root directory of the repo.
+# 2. Before using the script, execute one of the following command:
+#    - `yarn add --dev eslint`
+#    - `npm install --save-dev eslint`
+# 3. If the repo is created by `create-react-app`, ignore #2.
 
-cd ${SCRIPT_PATH}
+set -e
+set -u
+SCRIPT_PATH=`cd "$(dirname "$0")"; pwd -P`
+
+eval cd ${SCRIPT_PATH}
 ./node_modules/eslint/bin/eslint.js "$@"
--- init.sh
+++ init.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
+set -e
 set -u
-SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+SCRIPT_PATH=`cd "$(dirname "$0")"; pwd -P`
 
 cd ${SCRIPT_PATH}
 git remote remove upstream
 git remote add upstream https://github.com/TeamYouthChina/frontend.git
 ./sync.sh
-rm -f ./.git/hooks/pre-commit
-cp ./git-hooks/pre-commit ./.git/hooks/pre-commit
 yarn install
